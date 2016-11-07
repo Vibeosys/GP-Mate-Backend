25df492142daf84465ac7929be8e2828e0823749
anandvb <anand@vibeosys.com>
2016-11-07 13:13:42 +0530

    Updated website changes
    Configurations UI, added attach files functionality and minor changes

--- src/main/java/ar/com/lodev/medical_manager/component/web/controller/DoctorAdminController.java
+++ src/main/java/ar/com/lodev/medical_manager/component/web/controller/DoctorAdminController.java
@@ -1,54 +1,111 @@
 package ar.com.lodev.medical_manager.component.web.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import ar.com.lodev.medical_manager.component.service.ConfigurationsService;
 import ar.com.lodev.medical_manager.component.service.PracticePlaceAssociationService;
 import ar.com.lodev.medical_manager.component.service.PracticePlaceService;
+import ar.com.lodev.medical_manager.model.Configurations;
 import ar.com.lodev.medical_manager.model.PracticePlaceDoctorRequestStatus;
+import ar.com.lodev.medical_manager.model.dto.ConfigurationsDTO;
 import ar.com.lodev.medical_manager.model.dto.DoctorPracticePlaceAssociationDTO;
 import ar.com.lodev.medical_manager.ui.model.DataTableData;
 
 @Controller
-@RequestMapping(value="/doctor-admin")
-public class DoctorAdminController extends BaseController{
-	
+@RequestMapping(value = "/doctor-admin")
+public class DoctorAdminController extends BaseController {
+
 	static final Logger logger = Logger.getLogger(DoctorAdminController.class);
-	
+
 	@Autowired
 	private PracticePlaceService practicePlaceService;
 	@Autowired
 	private PracticePlaceAssociationService practicePlaceAssociationService;
-	
-	@RequestMapping(value="/list-doctors" , method=RequestMethod.GET)
-	public String dashboard(Model model){
+
+	@Autowired
+	private ConfigurationsService configurationsService;
+
+	@RequestMapping(value = "/list-doctors", method = RequestMethod.GET)
+	public String dashboard(Model model) {
 		model.addAttribute("section", PANEL_SECTION_DOCTORS);
 		return "practice-admin/list-doctors";
 	}
-	
-	@RequestMapping(value="/json/doctor/list", method=RequestMethod.GET)
-	public @ResponseBody DataTableData listJson(@RequestParam int start,@RequestParam long draw){
-		List<DoctorPracticePlaceAssociationDTO> dtos = practicePlaceService.listDoctors(start);
-		long recordsTotal = practicePlaceService.countDoctors();
-		return new DataTableData(draw,recordsTotal, recordsTotal, dtos);
+
+	@RequestMapping(value = "/list-doctors1", method = RequestMethod.GET)
+	public String dashboard1(Model model) {
+		model.addAttribute("section", PANEL_SECTION_CONFIGURATIONS);
+		return "practice-admin/configurations";
 	}
-	
-	@RequestMapping(value="/json/request/updateStatus", method=RequestMethod.POST)
-	public @ResponseBody DoctorPracticePlaceAssociationDTO updateRequestStatus(@RequestParam long associationId,
-			@RequestParam String status) throws Exception{
-		try{
-			PracticePlaceDoctorRequestStatus statusEnum = PracticePlaceDoctorRequestStatus.valueOf(status);
-			DoctorPracticePlaceAssociationDTO dto = practicePlaceAssociationService.updateRequestStatus(associationId, statusEnum);
+
+	@RequestMapping(value = "/json/doctor/list", method = RequestMethod.GET)
+	public @ResponseBody
+	DataTableData listJson(@RequestParam int start, @RequestParam long draw) {
+		List<DoctorPracticePlaceAssociationDTO> dtos = practicePlaceService
+				.listDoctors(start);
+		long recordsTotal = practicePlaceService.countDoctors();
+		return new DataTableData(draw, recordsTotal, recordsTotal, dtos);
+	}
+
+	@RequestMapping(value = "/json/doctor/list1", method = RequestMethod.GET)
+	public @ResponseBody
+	DataTableData listJson1(@RequestParam int start, @RequestParam long draw) {
+		List<ConfigurationsDTO> configDtoList = configurationsService.findAll();
+		return new DataTableData(draw, configDtoList.size(),
+				configDtoList.size(), configDtoList);
+
+	}
+
+	@RequestMapping(value = "/configurations1", method = RequestMethod.POST)
+	public @ResponseBody
+	boolean setConfigurations1(@RequestParam String key,
+			@RequestParam String value) {
+
+		List<ConfigurationsDTO> configDtoList = new ArrayList<ConfigurationsDTO>();
+		if (StringUtils.isNotEmpty(StringUtils.trim(key))) {
+			ConfigurationsDTO configDTO = configurationsService
+					.findByKey(StringUtils.trim(key));
+			if (configDTO != null) {
+				configDtoList.add(configDTO);
+			}
+
+			List<Configurations> configurations = new ArrayList<Configurations>();
+			if (CollectionUtils.isNotEmpty(configDtoList)) {
+				for (ConfigurationsDTO confDto : configDtoList) {
+					Configurations config = new Configurations(confDto);
+					configurations.add(config);
+				}
+				configurationsService.update(configurations);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@RequestMapping(value = "/json/request/updateStatus", method = RequestMethod.POST)
+	public @ResponseBody
+	DoctorPracticePlaceAssociationDTO updateRequestStatus(
+			@RequestParam long associationId, @RequestParam String status)
+			throws Exception {
+		try {
+			PracticePlaceDoctorRequestStatus statusEnum = PracticePlaceDoctorRequestStatus
+					.valueOf(status);
+			DoctorPracticePlaceAssociationDTO dto = practicePlaceAssociationService
+					.updateRequestStatus(associationId, statusEnum);
 			return dto;
-		}catch(Exception e){
+		} catch (Exception e) {
 			logger.error(ERROR, e);
 			throw new Exception(e.getMessage());
 		}
--- src/main/webapp/js/practice-admin/list-sessions.js
+++ src/main/webapp/js/practice-admin/list-sessions.js
@@ -71,6 +71,7 @@ function buildDataForClipboard(){
 }
 
 $("#table-panel #export").click(function(e){
+	$("#sessionToClipboard").prepend("<H2>Prior to consultation, the patient used the GP-Mate app to report the following:<H2><br/>");
 	$("#sessionToClipboard").show();
 	
 });
@@ -326,7 +327,7 @@ function buildSessionTable(){
 		           "mData": null,
 		           "width": "15%",
 		           "mRender": function(data, type, full) {
-		        	   return new Date(data.startDate).toString('MMMM d : HH:mm');
+		        	   return new Date(data.startDate).toString('MM-d-yyyy : HH:mm');
 		           }
 		        },
 				{
@@ -339,7 +340,7 @@ function buildSessionTable(){
 		           "mData": null,
 		           "width": "15%",
 		           "mRender": function(data, type, full) {
-		        	   return new Date(data.patient.dateOfBirth).toString('MMMM d, yyyy');
+		        	   return new Date(data.patient.dateOfBirth).toString('MM-d-yyyy');
 		           }
 		        },
 		        {
@@ -361,7 +362,7 @@ function buildSessionTable(){
 			    {
 			           "mData": null,
 			           "mRender": function(data, type, full) {
-			        	   return new Date(data.patient.appointmentDate).toString('MMMM d, yyyy : HH:mm');
+			        	   return new Date(data.patient.appointmentDate).toString('MM-d-yyyy : HH:mm');
 			           }
 			        }
 			]
