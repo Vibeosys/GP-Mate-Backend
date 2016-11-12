51c2f51cc3c5fd8d3876eae8ddefb8f4ff7716aa
anandvb <anand@vibeosys.com>
2016-11-12 21:26:43 +0530

    Updated database scripts
    For config and practice id

--- src/main/resources/database-scripts/initial-data-20161030.sql
+++ src/main/resources/database-scripts/initial-data-20161030.sql
@@ -1,4 +1,5 @@
 LOCK TABLES `configurations` WRITE;
 INSERT INTO `medical-manager`.`configurations` (`key_col`, `value_col`) VALUES ('maxNoOfProblemBoxes', '4');
 INSERT INTO `medical-manager`.`configurations` (`key_col`, `value_col`) VALUES ('timeInMinutes', '15');
+INSERT INTO `medical-manager`.`configurations` (`key_col`, `value_col`) VALUES ('genericEmailAddress', 'no-reply@gpmate.co.uk');
 UNLOCK TABLES;
